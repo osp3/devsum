5a0735682940ded350c2c474596bec24d7325cc9
ysabogal88 <yurisabogal@icloud.com>
2025-07-09 12:15:17 -0400

    feat(tomorrow priority done)

--- frontend/src/components/TomorrowsPriorities.jsx
+++ frontend/src/components/TomorrowsPriorities.jsx
@@ -5,21 +5,27 @@ const TomorrowsPriorities = () => {
 
 
 
-  return(
-     <div className ='' >
-    <div className = 'flex justify-center  text-[#5b56dd] text-2xl'>
+  return (
+    <div className=''>
+      <div className='flex justify-center  text-[#5b56dd] text-2xl'>
         <h1>Tomorrow's Priorities </h1>
-    </div>
-    <div className = 'm-4'>
+      </div>
+      <div className='m-3'>
+        <ul className='flex flex-col gap-3  items-start  list-disc rounded-lg text-[#5d58da]  '>
+          <li className ='bg-[#272633] border border-[#5d58da] rounded-lg p-3 text-[#5d58da] '> 
+             <span className="text-3xl flex-row item-center text-white ">•</span>
+            <span> Start working on analysis page</span>
+            
+            </li>
 
-<ul className ="list-disc flex flex-row justify-between items-center  list-disc rounded-lg text-[#5d58da] bg-[#272633] shadow-[0px_0_0_0px]  m-4">
-  <li> List of things to do</li>
- 
-</ul>
-
+          <li className='bg-[#272633] border border-[#5d58da] rounded-lg p-3 text-[#5d58da]'>  
+            List of things to do tomorrow morning</li>
+        </ul>
+      </div>
     </div>
-  
-  </div>);
+  );
 };
 
 export default TomorrowsPriorities;
+
+//bullet point outside or inside
