858f0bafb9d23ac45d3d87dfe9038067715731c5
ysabogal88 <yurisabogal@icloud.com>
2025-07-09 16:02:05 -0400

    refactor(need to fix resizing

--- frontend/src/components/ShowReposButton.jsx
+++ frontend/src/components/ShowReposButton.jsx
@@ -3,16 +3,22 @@ import React from 'react';
 const ShowReposButton = () => {
  
  // const [button, setButton]= useState('')
+const handleClick = () =>{
+ 
+    window.location.href = 'http://localhost:5173/repositories';
 
+}
 
   return( 
   <div
-  className = 'background-color: var(--color-white)'
+  className = 'flex justify-center '
   >
-    <h1>this is the get repo button </h1>
+  
 
-    <button className = 'bg-grey boarder-2 boarder-black flex items-center'> 
-      <spam>Git repo</spam>
+    <button className = 'bg-[#262728] boarder-2 boarder-black btn flex items-center m-5'
+    onClick={handleClick}
+    > 
+      <spam>GitHub Repos</spam>
       </button>
   </div>
   )
