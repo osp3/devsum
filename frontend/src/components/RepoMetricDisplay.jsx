0271c98c18387082fa97d77ea9172df2a57cbd42
ysabogal88 <yurisabogal@icloud.com>
2025-07-11 11:28:10 -0400

    bugfix(repoMetricDisplay): add extra div

--- frontend/src/components/RepoMetricDisplay.jsx
+++ frontend/src/components/RepoMetricDisplay.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 const RepoMetricDisplay = () => {
 
 
-  return( <div className= 'flex flex-row '>
+  return(
+    <div className= 'flex flex-row '>
 <div className = 'flex-1 min-w-30 flex-item-center border border-slate-400 rounded-lg m-4 '> 
   <h1 className = 'flex justify-center m-2'>6</h1>
-  <hi className = 'flex justify-center m-2'>Total commits </hi>
+  <h1 className = 'flex justify-center m-2'>Total commits </h1>
 </div>
     <div className = 'flex-1 min-w-30 flex-item-center border border-slate-400   rounded-lg m-4 '>
       <h1 className = 'flex justify-center m-2'>6</h1>
@@ -14,13 +15,14 @@ const RepoMetricDisplay = () => {
 
   
     </div>
+    </div>
   );
 };
 
 export default RepoMetricDisplay;
 
-//working on centering everything in individual boxes
+{/* //working on centering everything in individual boxes
 
 
 //flex1 makes each div take equal spaces , min-w-5 set a min with to the box
-
+ */}
