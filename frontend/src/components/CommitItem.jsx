84e7e7137c799007cee3a089cb0d0023099176fa
ysabogal88 <yurisabogal@icloud.com>
2025-07-14 15:20:02 -0400

    feat(commitItem)added individal style to commits

--- frontend/src/components/CommitItem.jsx
+++ frontend/src/components/CommitItem.jsx
@@ -39,12 +39,31 @@ const CommitItem = ({ commit, selectedRepo }) => {
   };
 
   // Placeholder render - actual commit item UI to be implemented
-  return <div>
+  return (<div>
 
     <h1>
       Button with color for every commit should be inside
     </h1>
-  </div>;
-};
+
+<div className = 'max-h-90 overflow-y-auto pr-3'>
+      <div className='relative flex flex-row justify-between items-center rounded-lg text-[#5b56dd] bg-[#272633] shadow-[-4px_0_0_0px] m-3'>
+        <button className='btn bg-[#44905e] text-white px-3 py-1 rounded'>
+          ${getCommitTypeColor(commit.message)} 
+        
+        </button>
+        <h1 className='flex-1 p-2 text-white'>{truncateMessage(commit.message)}</h1>
+        <span className='absolute top-2 right-2 text-xs text-gray-500'>
+          date
+        </span>
+
+</div>
+
+</div>
+ 
+
+
+    
+  </div>
+)};
 
 export default CommitItem;
