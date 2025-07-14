28450c743a5fdf4aab435943c82669698312ea26
ysabogal88 <yurisabogal@icloud.com>
2025-07-14 14:15:57 -0400

    working on recentCommits

--- frontend/src/components/RecentCommits.jsx
+++ frontend/src/components/RecentCommits.jsx
@@ -2,19 +2,96 @@ import React from 'react';
 import CommitItem from './CommitItem.jsx';
 
 const RecentCommits = () => {
+
+
+// const RecentCommits = ({ commits }) => {
+//   const actualCommits = commits.filter(commit => !commit.isAI);
+//   const aiCommits = commits.filter(commit => commit.isAI);
+
+
   return (
     <div className='p4'>
-      <div className='flex justify-items-start font-bold text-white'>
+      {/* sets reccent Commit to the right */}
+      <div className='flex  justify-items-start font-bold text-white'>
        Recent Commits
-      </div>
+
+
+ </div>
+ <div className = 'max-h-90 overflow-y-auto pr-3'>
       <div className='relative flex flex-row justify-between items-center rounded-lg text-[#5b56dd] bg-[#272633] shadow-[-4px_0_0_0px] m-3'>
         <button className='btn bg-[#44905e] text-white px-3 py-1 rounded'>
           {} repos
         </button>
-        <h1 className='flex-1 p-2 text-white'>{}</h1>
+        <h1 className='flex-1 p-2 text-white'>actual commit message</h1>
         <span className='absolute top-2 right-2 text-xs text-gray-500'>
           date
         </span>
+
+</div>
+
+</div>
+ <div className = 'max-h-90 overflow-y-auto pr-3'>
+      <div className='relative flex flex-row justify-between items-center rounded-lg text-[#5b56dd] bg-[#272633] shadow-[-4px_0_0_0px] m-3'>
+        <button className='btn bg-[#44905e] text-white px-3 py-1 rounded'>
+          {} repos
+        </button>
+        <h1 className='flex-1 p-2 text-white'>actual commit message</h1>
+        <span className='absolute top-2 right-2 text-xs text-gray-500'>
+          date
+        </span>
+
+</div>
+</div>
+ <div className = 'max-h-90 overflow-y-auto pr-3'>
+      <div className='relative flex flex-row justify-between items-center rounded-lg text-[#5b56dd] bg-[#272633] shadow-[-4px_0_0_0px] m-3'>
+        <button className='btn bg-[#44905e] text-white px-3 py-1 rounded'>
+          {} repos
+        </button>
+        <h1 className='flex-1 p-2 text-white'>actual commit message</h1>
+        <span className='absolute top-2 right-2 text-xs text-gray-500'>
+          date
+        </span>
+
+</div>
+</div>
+ <div className = 'max-h-90 overflow-y-auto pr-3'>
+      <div className='relative flex flex-row justify-between items-center rounded-lg text-[#5b56dd] bg-[#272633] shadow-[-4px_0_0_0px] m-3'>
+        <button className='btn bg-[#44905e] text-white px-3 py-1 rounded'>
+          {} repos
+        </button>
+        <h1 className='flex-1 p-2 text-white'>actual commit message</h1>
+        <span className='absolute top-2 right-2 text-xs text-gray-500'>
+          date
+        </span>
+
+</div>
+</div>
+ <div className = 'max-h-90 overflow-y-auto pr-3'>
+      <div className='relative flex flex-row justify-between items-center rounded-lg text-[#5b56dd] bg-[#272633] shadow-[-4px_0_0_0px] m-3'>
+        <button className='btn bg-[#44905e] text-white px-3 py-1 rounded'>
+          {} repos
+        </button>
+        <h1 className='flex-1 p-2 text-white'>actual commit message
+          
+        </h1>
+        <span className='absolute top-2 right-2 text-xs text-gray-500'>
+          date
+        </span>
+
+</div>
+</div>
+ <div className = 'max-h-90 overflow-y-auto pr-3'>
+      <div className='relative flex flex-row justify-between items-center rounded-lg text-[#5b56dd] bg-[#272633] shadow-[-4px_0_0_0px] m-3'>
+        <button className='btn bg-[#44905e] text-white px-3 py-1 rounded'>
+          {} repos
+        </button>
+        <h1 className='flex-1 p-2 text-white'>actual commit message</h1>
+        <span className='absolute top-2 right-2 text-xs text-gray-500'>
+          date
+        </span>
+
+</div>
+
       </div>
     </div>
 
--- frontend/src/components/TomorrowsPriorities.jsx
+++ frontend/src/components/TomorrowsPriorities.jsx
@@ -39,10 +39,12 @@ const TomorrowsPriorities = ({ recentCommits, repositoryId }) => {
       <div className='flex justify-center  text-[#5b56dd] text-2xl'>
         <h1>Tomorrow's Priorities </h1>
       </div>
+
+      
       <div className='m-3'>
         <ul className='flex flex-col gap-3  items-start rounded-lg text-[#5d58da]  '>
           <li className='bg-[#272633] border border-[#5d58da] rounded-lg p-3 text-[#FFFFFF] '>
-            <span className='text-3xl flex-row item-center text-white '></span>
+         
             <span> Start working on analysis page</span>
           </li>
 
