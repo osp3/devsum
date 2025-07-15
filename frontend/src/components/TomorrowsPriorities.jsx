fe44308b98633c6e82b7ddf35b441193f730a6b3
ysabogal88 <yurisabogal@icloud.com>
2025-07-15 18:50:49 -0400

    docs(tomorrorsPriorities) added pseudocode

--- frontend/src/components/TomorrowsPriorities.jsx
+++ frontend/src/components/TomorrowsPriorities.jsx
@@ -11,34 +11,33 @@ const TomorrowsPriorities = ({
   if(tasksLoading)return <div className='p-4 text-white'>Loading tasks...</div>
   if(tasksError) return <div className= 'p-4 text-white'>Error:{tasksError}</div>
  if(!taskSuggestions)return <div className = 'p4 text-white'>no task available</div>
- // Access tasks from the nested data structure
+ //extract task from data structure in app.jsx 
   const tasks = taskSuggestions;
   console.log(tasks)
-
+// checks that  there is data 
  if (!tasks || !Array.isArray(tasks) || tasks.length === 0) 
     return <div className='p-4 text-gray-400'>No tasks found in suggestions</div>;
+//renders the first container
   return (
       <div className='p-2'>
       <div className='flex justify-items-start font-bold text-white'>
        Todays Priorities
       </div>
-      
+      {/* add a vertical scrollable bar with a height of 130 */}
       <div className='max-h-130 overflow-y-auto pr-2'>
-        
+        {/* iterate on each task */}
         {tasks.map((task, index) => (
           <div key={index} className='mb-4'>
         
             {/* Task Details */}
-            
+            {/*  */}
             <div className='
             flex flex-col bg-[#272633] rounded-lg p-3 border-l-4 border-[#5b56dd]'>
+              {/* gives task title in white  */}
               <h2 className='text-white font-semibold text-sm '>{task.title}</h2>
-              <p className='text-white text-sm mb-3 p-3'>{task.description}</p>
+              {/* gives task description added a padding of 3 so that is shows indented from the title */}
+              <p className='text-white text-sm  p-3'>{task.description}</p>
               
-              <div className='flex items-center gap-4 text-xs text-gray-400'>
-               
-             
-              </div>
             </div>
           </div>
         ))}
